2ba9179f6a234c001b032344b48895419609c2e1
Lucas Srigley <lsrigley97@gmail.com>
2025-07-22 18:01:36 -0400

    check if db connection is problem

--- index.js
+++ index.js
@@ -15,31 +15,34 @@ const uri = process.env.MONGO_URI; //loading the full mongoDB connection string 
 const port = process.env.PORT || 8000;  //defines the port that the express server will be listening on
 
 //trying to connect to MongoDB with uri (which is the connection string)
-MongoClient.connect(
-    uri, 
-    {
-        maxPoolSize: 50,    //maximum number of concurrent DB connections (for performance)
-        wtimeoutMS: 2500,   //write timeout if mongo doesnt respond in time
-    }) 
-    /* using MongoClient.connect() is a function that will return a promise. This doesn't mean it 
-       returns the final result, but rather, it will return a promise object that represents the 
-       eventual completion or failure of the asynchronous operation. Then we will need to have code
-       for a .catch() or .then() methods to handle the result or error once the operation is done */ 
+// MongoClient.connect(
+//     uri, 
+//     {
+//         maxPoolSize: 50,    //maximum number of concurrent DB connections (for performance)
+//         wtimeoutMS: 2500,   //write timeout if mongo doesnt respond in time
+//     }) 
+//     /* using MongoClient.connect() is a function that will return a promise. This doesn't mean it 
+//        returns the final result, but rather, it will return a promise object that represents the 
+//        eventual completion or failure of the asynchronous operation. Then we will need to have code
+//        for a .catch() or .then() methods to handle the result or error once the operation is done */ 
 
-    //if the MongoDB connection fails, then we're going to...
-    .catch(err => {
-        console.error(err.stack) //we're going to log the error stack to the console
-        process.exit(1) //exit the app with status code 1 (indicating failure)
-    })
+//     //if the MongoDB connection fails, then we're going to...
+//     .catch(err => {
+//         console.error(err.stack) //we're going to log the error stack to the console
+//         process.exit(1) //exit the app with status code 1 (indicating failure)
+//     })
 
-    //if the MongoDB connection succeeds, then...
-    .then(async client => { 
-        await CourseReviewsDAO.injectDB(client);
-        await ProfessorReviewsDAO.injectDB(client);
-        await CoursesDAO.injectDB(client); //injecting the MongoDB client into the DAO classes so they can use it to access the database
-        await ProfessorsDAO.injectDB(client);
-        app.listen(port, () => { //we're going to start the Express server using app.listen (app comes from server.js remember we imported it)
-            console.log(`listening on port ${port}`) //log a message saying the server is up
-        })
-    }) 
+//     //if the MongoDB connection succeeds, then...
+//     .then(async client => { 
+//         await CourseReviewsDAO.injectDB(client);
+//         await ProfessorReviewsDAO.injectDB(client);
+//         await CoursesDAO.injectDB(client); //injecting the MongoDB client into the DAO classes so they can use it to access the database
+//         await ProfessorsDAO.injectDB(client);
+//         app.listen(port, () => { //we're going to start the Express server using app.listen (app comes from server.js remember we imported it)
+//             console.log(`listening on port ${port}`) //log a message saying the server is up
+//         })
+//     }) 
 
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
